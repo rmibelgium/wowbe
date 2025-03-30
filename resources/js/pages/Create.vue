38a41e26e4a15d0749b34979ad9b8e854b493d8c
Jonathan Beliën <dev@jbelien.mozmail.com>
2025-03-30 14:55:00 +0200

    Cleanup

--- resources/js/pages/Create.vue
+++ resources/js/pages/Create.vue
@@ -1,20 +1,4 @@
 <script setup lang="ts">
-// import AppLayout from '@/layouts/AppLayout.vue';
-// import { type BreadcrumbItem } from '@/types';
-// import { Head } from '@inertiajs/vue3';
-// import PlaceholderPattern from '../components/PlaceholderPattern.vue';
-// import { Button } from '@/components/ui/button'
-import { FormItem } from '@/components/ui/form';
-// import { Input } from '@/components/ui/input'
-// import { toast } from '@/components/ui/toast'
-
-// import { toTypedSchema } from '@vee-validate/zod'
-// import { useForm } from 'vee-validate'
-// import { h } from 'vue'
-// import * as z from 'zod'
-// import Toaster from '@/components/ui/toast/Toaster.vue'
-// import { router } from '@inertiajs/vue3'
-
 import InputError from '@/components/InputError.vue';
 import { Button } from '@/components/ui/button';
 import {
@@ -28,6 +12,7 @@ import {
     ComboboxList,
     ComboboxTrigger,
 } from '@/components/ui/combobox';
+import { FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { PinInput, PinInputGroup, PinInputInput } from '@/components/ui/pin-input';
@@ -61,7 +46,6 @@ const form = useForm({
 const submit = () => {
     form.post(route('site.store'), {
         onSuccess: (values) => {
-            console.log(values.rememberedState);
             toast({
                 title: 'Site created',
                 description: `The site "" has been created successfully.`,
@@ -70,35 +54,6 @@ const submit = () => {
         // onFinish: () => form.reset('password', 'password_confirmation'),
     });
 };
-
-// const formSchema = toTypedSchema(z.object({
-//     longitude: z.number().min(-180).max(180),
-//     latitude: z.number().min(-90).max(90),
-//     height: z.number().int().min(0),
-//     name: z.string(),
-//     timezone: z.string(),
-//     auth_key: z.string().length(6),
-// }))
-
-// const { isFieldDirty, handleSubmit } = useForm({
-//     validationSchema: formSchema,
-// })
-
-// const onSubmit = handleSubmit((values) => {
-//     console.log(values);
-//     router.post(route('api.site.store'), values, {
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         onSuccess: (site) => {
-//             console.log(site)
-//             toast({
-//                 title: 'Site created',
-//                 description: `The site "" has been created successfully.`,
-//             })
-//         },
-//     })
-// })
 </script>
 
 <template>
@@ -198,143 +153,6 @@ const submit = () => {
                     </PinInput>
                     <InputError :message="form.errors.auth_key" />
                 </FormItem>
-
-                <!--
-
-                <FormField v-slot="{ componentField }" name="longitude" :validate-on-blur="!isFieldDirty">
-                    <FormItem v-auto-animate>
-                        <FormLabel>Longitude</FormLabel>
-                        <FormControl>
-                            <Input type="number" step="0.000001" v-bind="componentField" />
-                        </FormControl>
-                        <FormDescription>
-                            This is your public display name.
-                        </FormDescription>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-
-                <FormField v-slot="{ componentField }" name="latitude" :validate-on-blur="!isFieldDirty">
-                    <FormItem v-auto-animate>
-                        <FormLabel>Latitude</FormLabel>
-                        <FormControl>
-                            <Input type="number" step="0.000001" v-bind="componentField" />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-
-                <FormField v-slot="{ componentField }" name="height" :validate-on-blur="!isFieldDirty">
-                    <FormItem v-auto-animate>
-                        <FormLabel>Height above sea level (Metres)</FormLabel>
-                        <FormControl>
-                            <Input type="number" v-bind="componentField" />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-
-                <Separator />
-
-                <div>
-                    <h3 class="text-lg font-medium">2. Site Details</h3>
-                    <p class="text-sm text-muted-foreground">Please enter either a postcode, location, or lat/lon
-                        values, to allow us to position your site on the map. Once you've entered a location, you may
-                        click and drag the pin to a more accurate location.</p>
-                </div>
-
-                <FormField v-slot="{ componentField }" name="name" :validate-on-blur="!isFieldDirty">
-                    <FormItem v-auto-animate>
-                        <FormLabel>Site Name</FormLabel>
-                        <FormControl>
-                            <Input type="text" v-bind="componentField" />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-
-                <FormField v-slot="{ componentField }" name="timezone" :validate-on-blur="!isFieldDirty">
-                    <FormItem v-auto-animate>
-                        <FormLabel>Site TimeZone</FormLabel>
-                        <FormControl>
-                            <Input type="text" v-bind="componentField" />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-
-                <FormField v-slot="{ componentField }" name="auth_key" :validate-on-blur="!isFieldDirty">
-                    <FormItem v-auto-animate>
-                        <FormLabel>Authentication Key</FormLabel>
-                        <FormControl>
-                            <Input type="text" minlength="6" maxlength="6" v-bind="componentField" />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-
-                <Separator />
-
-                <div>
-                    <h3 class="text-lg font-medium">3. Site Owner Details</h3>
-                    <p class="text-sm text-muted-foreground">Here you may provide some details about the site owner, such as the organisation they belong to or if their Site has a website.</p>
-                </div>
-
-                <FormField v-slot="{ componentField }" name="website" :validate-on-blur="!isFieldDirty">
-                    <FormItem v-auto-animate>
-                        <FormLabel>Add related website to your observation</FormLabel>
-                        <FormControl>
-                            <Input type="text" v-bind="componentField" />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-
-                <FormField v-slot="{ componentField }" name="organization" :validate-on-blur="!isFieldDirty">
-                    <FormItem v-auto-animate>
-                        <FormLabel>Org/School Name</FormLabel>
-                        <FormControl>
-                            <Input type="text" v-bind="componentField" />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-
-                <FormField v-slot="{ componentField }" name="reason" :validate-on-blur="!isFieldDirty">
-                    <FormItem v-auto-animate>
-                        <FormLabel>Reasons for running the site?</FormLabel>
-                        <FormControl>
-                            <Input type="text" v-bind="componentField" />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-
-                <FormField v-slot="{ componentField }" name="access" :validate-on-blur="!isFieldDirty">
-                    <FormItem v-auto-animate>
-                        <FormLabel>Default Access Level</FormLabel>
-                        <FormControl>
-                            <Input type="text" v-bind="componentField" />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                </FormField>
-
-                <Separator />
-
-                <div>
-                    <h3 class="text-lg font-medium">4. Site Data Preferences</h3>
-                    <p class="text-sm text-muted-foreground">Please describe the location attributes of the observation equipment at this site.</p>
-                </div>
-
-                <div data-orientation="horizontal" role="separator" class="shrink-0 bg-border relative h-px w-full my-6"></div>
-
-                <div>
-                    <h3 class="text-lg font-medium">5. Additional Information</h3>
-                    <p class="text-sm text-muted-foreground">Please enter any additional information about this site and its equipment (maximum 500 characters)</p>
-                </div>
-
-                -->
 
                 <Separator />
 
