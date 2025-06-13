dd5af3359338bbefcc9bd1be603a2f980241664e
Jonathan Beliën <dev@jbelien.mozmail.com>
2025-06-13 14:05:53 +0200

    💄 Remove register button from homepage

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -57,12 +57,12 @@ const handleFeatureClick = (feature: GeoJSONFeature) => {
                     >
                         Log in
                     </Link>
-                    <Link
+                    <!-- <Link
                         :href="route('register')"
                         class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                     >
                         Register
-                    </Link>
+                    </Link> -->
                 </template>
             </nav>
         </header>
