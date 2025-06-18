191c5fa15d8529d659213b7a03ec3d7883e7c123
Jonathan Beliën <dev@jbelien.mozmail.com>
2025-06-18 10:48:00 +0200

    🚨 Apply Pint fixes

--- app/Helpers/ObservationHelper.php
+++ app/Helpers/ObservationHelper.php
@@ -8,7 +8,7 @@ class ObservationHelper
 {
     /**
      * Serialize the observation date in the right format.
-     * Force to array to ensure correct serialization 
+     * Force to array to ensure correct serialization
      * (see `serializeDate()` function in `Observation` model).
      */
     public static function serializeDateUTC(Observation $observation): string
