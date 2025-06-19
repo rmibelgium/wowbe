d70d60143015f8b559fbc6d8754438f4f02b2c1f
Jonathan Beliën <dev@jbelien.mozmail.com>
2025-06-19 14:18:42 +0200

    🚚 Move map out of homepage

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,11 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+Route::redirect('/', 'dashboard');
+
+Route::get('/map', function () {
+    return Inertia::render('Map');
+})->name('map');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', DashboardController::class)->name('dashboard');
