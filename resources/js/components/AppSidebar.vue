52373416544f3aac663c5bd52f32d79daf04c510
Jonathan Beliën <dev@jbelien.mozmail.com>
2025-06-23 22:04:37 +0200

    🚨 Apply Prettier fixes

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, CirclePlus, Folder, LayoutGrid } from 'lucide-vue-next';
+import { CirclePlus, Folder, LayoutGrid } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
