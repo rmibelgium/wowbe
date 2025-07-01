ba50ea4e205b9a61029d82bf4ae23523b000793f
Jonathan Beliën <dev@jbelien.mozmail.com>
2025-07-01 11:52:43 +0200

    ✨ Update daily MV hourly

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schedule;
+
+// Commands for the Artisan console
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('db:refresh-daily', function () {
+    DB::statement('REFRESH MATERIALIZED VIEW CONCURRENTLY daily');
+})->purpose('Refresh "daily" materialized view');
+
+// Task Scheduling
+
+Schedule::command('db:refresh-daily')
+    ->hourly()
+    ->withoutOverlapping();
