5b141830b8df6e847e007494f0c6f18169cbb290
Jonathan Beliën <dev@jbelien.mozmail.com>
2025-07-15 09:23:45 +0200

    🚨 Apply Pint fixes

--- database/migrations/2025_07_14_164018_use_sql_files_for_materialized_views.php
+++ database/migrations/2025_07_14_164018_use_sql_files_for_materialized_views.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
@@ -13,11 +11,11 @@ return new class extends Migration
     public function up(): void
     {
         DB::statement('DROP MATERIALIZED VIEW IF EXISTS observations_day_agg;');
-        DB::statement(file_get_contents(__DIR__ . '/sql/observations_day_agg.sql'));
+        DB::statement(file_get_contents(__DIR__.'/sql/observations_day_agg.sql'));
         DB::statement('CREATE UNIQUE INDEX observations_day_agg_unique_idx ON observations_day_agg(date, site_id);');
 
         DB::statement('DROP MATERIALIZED VIEW IF EXISTS observations_5min_agg;');
-        DB::statement(file_get_contents(__DIR__ . '/sql/observations_5min_agg.sql'));
+        DB::statement(file_get_contents(__DIR__.'/sql/observations_5min_agg.sql'));
         DB::statement('CREATE UNIQUE INDEX observations_5min_agg_unique_idx ON observations_5min_agg(datetime, site_id);');
     }
 
