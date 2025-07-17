2e8c5d19d5cc50cb0cbe6feab1b013643c270f1d
Jonathan Beliën <dev@jbelien.mozmail.com>
2025-07-17 13:44:01 +0200

    🚚 Update site routing

--- routes/site.php
+++ routes/site.php
@@ -7,12 +7,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('site/register', [SiteController::class, 'create'])->name('site.create');
     Route::post('site/register', [SiteController::class, 'store'])->name('site.store');
 
-    Route::get('site/edit/{site}', [SiteController::class, 'edit'])->name('site.edit');
-    Route::post('site/edit/{site}', [SiteController::class, 'update'])->name('site.update'); // Using POST to allow file uploads
+    Route::get('site/{site}/edit', [SiteController::class, 'edit'])->name('site.edit');
+    Route::post('site/{site}/edit', [SiteController::class, 'update'])->name('site.update'); // Using POST to allow file uploads
 
-    Route::get('site/auth/{site}', [SiteController::class, 'editAuth'])->name('site.edit_auth');
-    Route::patch('site/auth/{site}', [SiteController::class, 'updateAuth'])->name('site.update_auth');
+    Route::get('site/{site}/auth', [SiteController::class, 'editAuth'])->name('site.edit_auth');
+    Route::patch('site/{site}/auth', [SiteController::class, 'updateAuth'])->name('site.update_auth');
 
-    Route::get('site/delete/{site}', [SiteController::class, 'delete'])->name('site.delete');
-    Route::delete('site/delete/{site}', [SiteController::class, 'destroy'])->name('site.destroy');
+    Route::get('site/{site}/delete', [SiteController::class, 'delete'])->name('site.delete');
+    Route::delete('site/{site}/delete', [SiteController::class, 'destroy'])->name('site.destroy');
 });
