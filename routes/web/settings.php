f96c6fcc9d7fed5a2e59dcc6cdf41a600911bbaa
Jonathan Beliën <dev@jbelien.mozmail.com>
2025-08-27 15:58:31 +0200

    🚚 Move wbe routes under `/web`

--- routes/web/settings.php
+++ routes/web/settings.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware('auth')->group(function () {
-    Route::redirect('settings', '/settings/profile');
+    Route::any('settings', fn () => redirect()->route('profile.edit'));
 
     Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -15,7 +15,5 @@ Route::middleware('auth')->group(function () {
     Route::get('settings/password', [PasswordController::class, 'edit'])->name('password.edit');
     Route::put('settings/password', [PasswordController::class, 'update'])->name('password.update');
 
-    Route::get('settings/appearance', function () {
-        return Inertia::render('settings/Appearance');
-    })->name('appearance');
+    Route::get('settings/appearance', fn () => Inertia::render('settings/Appearance'))->name('appearance');
 });
